03c4a2e45722c7e38f477ea16af12ae073eb4a6e
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-03-12 17:37:06 +0100

    Initial Gradle runner implementation.

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,39 +1,85 @@
 package runner
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/executor"
 	"github.com/kubeshop/testkube/pkg/executor/output"
 )
 
+type Params struct {
+	Datadir string // RUNNER_DATADIR
+}
+
 func NewRunner() *GradleRunner {
-	return &GradleRunner{}
+	params := Params{
+		Datadir: os.Getenv("RUNNER_DATADIR"),
+	}
+
+	runner := &GradleRunner{
+		params: params,
+	}
+
+	return runner
 }
 
 type GradleRunner struct {
+	params Params
 }
 
 func (r *GradleRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
+	// check that the datadir exists
+	_, err = os.Stat(r.params.Datadir)
+	if errors.Is(err, os.ErrNotExist) {
+		return result, err
+	}
+
 	// the Gradle executor does not support files
 	if execution.Content.IsFile() {
-		output.PrintEvent("using file", execution)
-		// TODO implement file based test content for string, git-file, file-uri
-		//      or remove if not used
+		return result.Err(fmt.Errorf("executor only support git-dir based test")), nil
 	}
 
-	if execution.Content.IsDir() {
-		output.PrintEvent("using dir", execution)
-		// TODO implement file based test content for git-dir
-		//      or remove if not used
+	// check settings.gradle or build.gradle files exist
+	directory := filepath.Join(r.params.Datadir, "repo")
+	settingsGradle := filepath.Join(directory, "settings.gradle")
+	_, err = os.Stat(settingsGradle)
+	if errors.Is(err, os.ErrNotExist) {
+		return result.Err(fmt.Errorf("no settings.gradle file found for test")), nil
 	}
 
-	// TODO run executor here
+	// determine the Gradle command to use
+	gradleCommand := "gradle"
+	gradleWrapper := filepath.Join(directory, "gradlew")
+	_, err = os.Stat(gradleWrapper)
+	if err == nil {
+		// then we use the wrapper instead
+		gradleCommand = "./gradlew"
+	}
 
-	// error result should be returned if something is not ok
-	// return result.Err(fmt.Errorf("some test execution related error occured"))
+	// prepare the ENVs to use during Gradle execution
+	envs := ""
+	for key, value := range execution.Envs {
+		env := fmt.Sprintf("%s=%s", key, value)
+		envs = envs + env + " "
+	}
 
-	// TODO return ExecutionResult
+	// pass additional executor arguments/flags to Gradle
+	args := []string{"--no-daemon"}
+	args = append(args, execution.Args...)
+
+	output.PrintEvent("Running", directory, envs+gradleCommand, args)
+	output, err := executor.Run(directory, envs+gradleCommand, args...)
+	if err != nil {
+		return result.Err(err), nil
+	}
+
 	return testkube.ExecutionResult{
-		Status: testkube.StatusPtr(testkube.SUCCESS_ExecutionStatus),
-		Output: "exmaple test output",
+		Status:     testkube.StatusPtr(testkube.SUCCESS_ExecutionStatus),
+		Output:     string(output),
+		OutputType: "text/plain",
 	}, nil
 }
